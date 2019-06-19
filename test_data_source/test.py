2baa1da18f24c7220e7429b2517382011b9a7be6
Aksel Slettemark <akselslettemark@gmail.com>
2019-06-19 09:39:22 +0200

    Morning meeting

--- test_data_source/test.py
+++ test_data_source/test.py
@@ -1,7 +1,6 @@
 import boto3
 import random
 from boto3.dynamodb.conditions import Key
-from boto3.dynamodb.types import Decimal
 from datetime import datetime as dt
 import json
 
@@ -17,7 +16,7 @@ def get_timestamp_random(timestamp=None, random_value=None):
     :return: timestamp in bits appended with some random bits as a Binary.
     """
 
-    def to_byte_array(number: int, bytes: int):
+    def to_byte_array(number: int, bytes: int = 16):
         return number.to_bytes(bytes, 'big')
 
     if timestamp is None:
@@ -29,7 +28,7 @@ def get_timestamp_random(timestamp=None, random_value=None):
     return to_byte_array((timestamp << 64) + random_value, 16)
 
 
-def get_range_timestamp_random(timestamp):
+def get_range_timestamp_random(timestamp: int):
     """
     :param timestamp: Unix timestamp
     :return: A range of possible values a timestamp can get from get_timestamp_random().
@@ -37,8 +36,8 @@ def get_range_timestamp_random(timestamp):
     """
 
     lowest = get_timestamp_random(timestamp, random_value=0)
-    highest = get_timestamp_random(timestamp, random_value=2 ** 64)
-    return [lowest, highest]
+    highest = get_timestamp_random(timestamp + 1, random_value=0)
+    return lowest, highest
 
 
 def insert_doc(type, data=None, timestamp=None):
@@ -63,6 +62,7 @@ def insert_doc(type, data=None, timestamp=None):
 def main():
     f = open("example_data.json")
     example_data = json.loads(f.read())
+    f.close()
     for example in example_data:
         insert_doc(type=example["type"], data=example["data"], timestamp=example["timestamp"])
 
