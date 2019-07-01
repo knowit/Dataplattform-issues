4270dfc18f12fc83535a3b0f0050295d26044a8b
Aksel Slettemark <akselslettemark@gmail.com>
2019-07-01 12:09:38 +0200

    Add custom data filters for data types

---
+++ lambda/ingest/filters.py
@@ -0,0 +1,13 @@
+import json
+
+
+def filter_github(data):
+    data_dict = json.loads(data)
+    if data_dict.get("repository") and data_dict["repository"].get("private"):
+        return None
+    return data
+
+
+filter = {
+    "GithubType": filter_github,
+}
