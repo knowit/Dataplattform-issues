dc4c8d38893d2ef581c2095ab5bfd6d3f7ddfc04
Aksel Slettemark <akselslettemark@gmail.com>
2019-07-01 12:30:28 +0200

    Add filter that removes Slack message text

--- lambda/ingest/filters.py
+++ lambda/ingest/filters.py
@@ -8,6 +8,18 @@ def filter_github(data):
     return data
 
 
+def filter_slack(data):
+    data_dict = json.loads(data)
+    if "text" in data_dict["event"]:
+        del data_dict["event"]["text"]
+    return json.dumps(data_dict)
+
+
+"""
+Map of optional filter functions for data types. Value is a function that returns a redacted
+version of the data point, or None if the data point should be ignored.
+"""
 filter = {
     "GithubType": filter_github,
+    "SlackType": filter_slack
 }
