caf1d7b436a4e82f8d8a944decdfe84ef050b498
Tinus Flagstad <tinus@flagstad.as>
2019-07-10 13:08:52 +0200

    Add the proper URL for the wiki

--- lambda/slack_response/test/test_slack_response.py
+++ lambda/slack_response/test/test_slack_response.py
@@ -21,7 +21,8 @@ def test_create_blocks():
                 "type": "mrkdwn",
                 "text": "Velg arrangementet du vil bruke knappen på."
                         "\n (Listen er hentet fra google calender, lag event der først om du ikke "
-                        "ser den på listen her.) (link til wiki-side på hvordan)"
+                        "ser den på listen her.)\n Les hvordan du kobler opp her: \n "
+                        "https://github.com/knowit/Dataplattform/wiki/Lage-et-arrangement-koblet-opp-mot-vurderingssystemet"
             }
         },
         {
