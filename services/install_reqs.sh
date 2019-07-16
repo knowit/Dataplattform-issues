07e422d62c24aac943c8422c73fa5e4cf60cceac
Aksel Slettemark <akselslettemark@gmail.com>
2019-07-16 13:31:39 +0200

    Update install_reqs.sh with events_slack_app requirements.

--- services/install_reqs.sh
+++ services/install_reqs.sh
@@ -1,7 +1,11 @@
 pip install -r requirements.txt
+pip install -r events_slack_app/slack_response/requirements.txt
 pip install common_layers/timestamp_random/python/
 pip install common_layers/slack_signature_validator/python/
 pip install ingest/ingest/
 pip install ingest/slack_ingest/
 pip install ingest/github_ingest/
 pip install fetch/get_docs/
+pip install events_slack_app/slack_response/
+pip install events_slack_app/slack_command/
+
