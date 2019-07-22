a6ca380ff0da0f626a16b424363a7607e8df74a3
Tinus Flagstad <tinus@flagstad.as>
2019-07-22 16:03:22 +0200

    Blog poller big refactor and extracting the information needed.

--- services/poller/blog/blog_poller.py
+++ services/poller/blog/blog_poller.py
@@ -2,23 +2,54 @@ import urllib.request
 import json
 from bs4 import BeautifulSoup
 
-headers = {
-    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
-}
-request = urllib.request.Request("https://knowitlabs.no/latest", headers=headers)
-response = urllib.request.urlopen(request)
-res = response.read().decode()
 
-soup = BeautifulSoup(res, "html.parser")
-scripts = soup.find_all("script")
-script = scripts[8].text
-starts_with = """// <![CDATA[
-window["obvInit"]("""
-ends_with = """)
-// ]]>"""
-if script.startswith(starts_with) and script.endswith(ends_with):
-    script = script[len(starts_with):-len(ends_with):]
-print(script)
-json = json.loads(script)
+def lambda_handler(event, context):
+    html = get_html_from_blog()
+    medium_data = get_medium_data_dict(html)
+    docs = create_docs(medium_data)
+    print(json.dumps(docs))
+    return True
 
-print(json)
+
+def create_docs(data):
+    docs = []
+    for post in data["posts"]:
+        doc = {
+            "title": post["title"],
+            "subtitle": post["virtuals"]["subtitle"],
+            "created": post["createdAt"]
+        }
+        creator_id = post["creatorId"]
+
+        # TODO: Figure out if this is inefficient.
+        doc["author"] = data["references"]["User"][creator_id]["name"]
+        docs.append(doc)
+
+    return docs
+
+
+def get_html_from_blog():
+    headers = {
+        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
+    }
+    request = urllib.request.Request("https://knowitlabs.no/latest", headers=headers)
+    response = urllib.request.urlopen(request)
+
+    return response.read().decode()
+
+
+def get_medium_data_dict(html):
+    soup = BeautifulSoup(html, "html.parser")
+    scripts = soup.find_all("script")
+    script = scripts[8].text
+
+    starts_with = """// <![CDATA[\nwindow["obvInit"]("""
+
+    ends_with = """)\n// ]]>"""
+
+    if script.startswith(starts_with) and script.endswith(ends_with):
+        script = script[len(starts_with):-len(ends_with):]
+    return json.loads(script)
+
+
+lambda_handler(None, None)
