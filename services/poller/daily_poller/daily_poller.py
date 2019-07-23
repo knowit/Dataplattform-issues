b957820c422e208d982966aa66365b63aeb4f552
Tinus Flagstad <tinus@flagstad.as>
2019-07-23 13:17:39 +0200

    Add some more try catching incase one of the daily pollings results in a crash

--- services/poller/daily_poller/daily_poller.py
+++ services/poller/daily_poller/daily_poller.py
@@ -1,11 +1,21 @@
 import blog_poller
 import ubw_poller
+import traceback
 
 
 def lambda_handler(event, context):
-    res = ubw_poller.poll()
-    res2 = blog_poller.poll()
-    if res and res2:
+    # This is a list of polling methods that should be run once every day.
+    pollings = [ubw_poller.poll, blog_poller.poll]
+    errors = 0
+    for poll in pollings:
+        try:
+            poll()
+        except:
+            # If one of the polling methods fails it should not stop the others from running.
+            traceback.print_exc()
+            errors += 1
+
+    if errors == 0:
         return {
             'statusCode': 200,
             'body': ''
