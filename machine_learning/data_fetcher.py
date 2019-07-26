b279661b9f96e9983257bfac8b662b26ec405b35
Tinus Flagstad <tinus@flagstad.as>
2019-07-26 09:50:23 +0200

    Add proof of concept regression learning

--- machine_learning/data_fetcher.py
+++ machine_learning/data_fetcher.py
@@ -33,7 +33,7 @@ class DataFetcher:
             DataFetcher.__connection = connection
 
     @staticmethod
-    def fetch_daily_data(date_to=None):
+    def fetch_data(date_to=None, days=1):
         if date_to is None:
             date_to = datetime.now()
         date_from = date_to - timedelta(days=1)
@@ -42,7 +42,9 @@ class DataFetcher:
         timestamp_to = date_to.timestamp()
         x_data = DataFetcher.fetch_x_data(timestamp_from, timestamp_to)
         y_data = DataFetcher.fetch_label(timestamp_from, timestamp_to)
-        return x_data, y_data
+        if y_data is None:
+            return None
+        return [x_data], [y_data]
 
     @staticmethod
     def fetch_x_data(timestamp_from, timestamp_to):
@@ -53,19 +55,25 @@ class DataFetcher:
             cursor.execute(sql_query, (timestamp_from, timestamp_to))
             query_result = cursor.fetchall()
             if len(query_result) > 0:
+                if "timestamp" in query_result[0]:
+                    for i in range(len(query_result)):
+                        time_of_day = DataFetcher.timestamp_to_time_of_day(query_result[i]["timestamp"])
+                        query_result[i]["time_of_day"] = time_of_day
+                        del query_result[i]["timestamp"]
                 results[data_type] = query_result
 
-        slack_sql = "SELECT `slack_timestamp`, `event_type`, `channel_name` FROM `SlackType` WHERE `timestamp`>%s and `timestamp` <%s"
+        # slack_sql = "SELECT COUNT(*) as `count`, `channel_name` FROM `SlackType` WHERE `timestamp`>%s and `timestamp` <%s and `channel_name` IS NOT NULL GROUP BY `channel_name` ORDER BY `count` desc"
+        slack_sql = "SELECT `timestamp` FROM `SlackType` WHERE `timestamp`>%s and `timestamp` <%s"
         execute_sql_query("SlackType", slack_sql)
 
-        github_sql = "SELECT `language`, `repository_name`, `commit_timestamp`, `ref` FROM `GithubType` WHERE `timestamp`>%s and `timestamp` <%s"
-        execute_sql_query("GithubType", github_sql)
-
-        event_sql = "SELECT `event_name`, `number_of_people`, `group` FROM `EventType` WHERE `timestamp`>%s and `timestamp` <%s"
-        execute_sql_query("EventType", event_sql)
-
-        event_rating_sql = "Select `event_name`, sum(button) / count(button) as `ratio` from `EventRatingType` where `timestamp`>%s and `timestamp`<%s GROUP BY `event_name`"
-        execute_sql_query("EventRatingType", event_rating_sql)
+        # github_sql = "SELECT `language`, `repository_name`, `timestamp`, `ref` FROM `GithubType` WHERE `timestamp`>%s and `timestamp` <%s"
+        # execute_sql_query("GithubType", github_sql)
+        #
+        # event_sql = "SELECT `event_name`, `number_of_people`, `group` FROM `EventType` WHERE `timestamp`>%s and `timestamp` <%s"
+        # execute_sql_query("EventType", event_sql)
+        #
+        # event_rating_sql = "Select `event_name`, sum(button) / count(button) as `ratio` from `EventRatingType` where `timestamp`>%s and `timestamp`<%s GROUP BY `event_name`"
+        # execute_sql_query("EventRatingType", event_rating_sql)
 
         return results
 
@@ -73,3 +81,19 @@ class DataFetcher:
     def fetch_label(timestamp_from, timestamp_to):
         # TODO: This should do a query similar to the event_rating query.
         return 5 / 6
+        # This should return None if no one voted this day.
+
+    @staticmethod
+    def timestamp_to_time_of_day(timestamp):
+        """
+        :param timestamp:
+        :return: Either 'early', 'midday' or 'late'.
+        """
+        dt_object = datetime.fromtimestamp(timestamp)
+        hour = dt_object.hour
+        if hour < 10:
+            return "early"
+        elif hour < 14:
+            return "midday"
+        else:
+            return "late"
--- machine_learning/learner.py
+++ machine_learning/learner.py
@@ -1,13 +1,128 @@
-import tensorflow
-from data_fetcher import DataFetcher
 from datetime import datetime
+
+import tensorflow
+import tensorflow_transform as tft
+import tensorflow_transform.beam as tft_beam
+from data_fetcher import DataFetcher
+from numpy import array
+from tensorflow_transform.tf_metadata import dataset_metadata
+from tensorflow_transform.tf_metadata import dataset_schema
+
+
+def preprocess(data):
+    earlies_normalized = tft.scale_to_0_1(data["earlies"])
+    middays_normalized = tft.scale_to_0_1(data["middays"])
+    lates_normalized = tft.scale_to_0_1(data["lates"])
+
+    out = {
+        "earlies_normalized": earlies_normalized,
+        "middays_normalized": middays_normalized,
+        "lates_normalized": lates_normalized
+    }
+    return out
+
+
+def process_slack_data(data):
+    earlies = 0
+    middays = 0
+    lates = 0
+    for slack_data in data["SlackType"]:
+        time_of_day = slack_data["time_of_day"]
+        if time_of_day == "early":
+            earlies += 1
+        elif time_of_day == "midday":
+            middays += 1
+        elif time_of_day == "late":
+            lates += 1
+    del data["SlackType"]
+    data["earlies"] = earlies
+    data["middays"] = middays
+    data["lates"] = lates
+    return data
+
+
+def baseline_model():
+    model = tensorflow.keras.Sequential()
+    model.add(tensorflow.keras.layers.Dense(1000, input_dim=3))
+    model.add(tensorflow.keras.layers.Dense(100))
+    model.add(tensorflow.keras.layers.Dense(10))
+    model.add(tensorflow.keras.layers.Dense(1, activation='sigmoid'))
+    model.compile(
+        optimizer="adam",
+        loss="mean_squared_error",
+        metrics=['accuracy'])
+
+    return model
+
+
+def transform_data(data):
+    """
+    :param data: A list of raw data.
+    :return: A numpy array of arrays of integers.
+    """
+    with tft_beam.Context(temp_dir="temp/"):
+        raw_data_metadata = dataset_metadata.DatasetMetadata(
+            dataset_schema.from_feature_spec({
+                'earlies': tensorflow.FixedLenFeature([], tensorflow.int64),
+                'middays': tensorflow.FixedLenFeature([], tensorflow.int64),
+                'lates': tensorflow.FixedLenFeature([], tensorflow.int64),
+            }))
+
+        transformed_dataset, transform_fn = (
+                (data, raw_data_metadata) | tft_beam.AnalyzeAndTransformDataset(preprocess))
+        transformed_data, transformed_metadata = transformed_dataset
+
+    # TODO: There should be an easier way to do this.
+    retransformed_data = []
+    for trans in transformed_data:
+        current = [trans["earlies_normalized"],
+                   trans["middays_normalized"],
+                   trans["lates_normalized"]]
+        retransformed_data.append(current)
+    return array(retransformed_data)
+
+
+def train(date=None, days=1):
+    if date is None:
+        date = datetime.now()
+
+    raw_x_data, y_data = DataFetcher.fetch_data(date)
+
+    if "SlackType" in raw_x_data[0]:
+        x_data = [process_slack_data(raw_x_data[0])]
+
+    # TODO: Remove hardcoded test-data.
+    x_data = [{'earlies': 10, 'middays': 20, 'lates': 25},
+              {'earlies': 50, 'middays': 70, 'lates': 49},
+              {'earlies': 112, 'middays': 99, 'lates': 109}]
+    y_data = [0.2, 0.6, 0.9]
+    # TODO: End of hardcoded test-data.
+    transformed_data = transform_data(x_data)
+
+    model = baseline_model()
+    model.fit(transformed_data, y_data, epochs=100)
+    return model
+
+
+def predict(model, data):
+    return model.predict(data)
 
 
 def main():
     # TODO: move all of this to amazon sagemaker
-    # Usually just use the date for today.
-    july24 = datetime(2019, 7, 24, 19, 23, 29)
-    print(DataFetcher.fetch_daily_data(july24))
+
+    july24 = datetime(2019, 7, 24, 22, 23, 29)
+    model = baseline_model()
+    model = train(july24)
+
+    raw_data = [{'earlies': 10, 'middays': 20, 'lates': 25},
+                {'earlies': 50, 'middays': 70, 'lates': 49},
+                {'earlies': 112, 'middays': 99, 'lates': 109}]
+
+    data = transform_data(raw_data)
+
+    prediction = predict(model, data)
+    print(prediction)
 
 
 if __name__ == '__main__':
